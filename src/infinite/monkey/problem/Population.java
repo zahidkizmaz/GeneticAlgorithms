1e1d090129f478a8532326743f165554f9bc6dde
Zahid KIZMAZ <mzkizmaz@gmail.com>
2018-01-19 02:12:25 +0300

    creatingMatingPool func added

--- src/infinite/monkey/problem/Population.java
+++ src/infinite/monkey/problem/Population.java
@@ -2,6 +2,8 @@
 package infinite.monkey.problem;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 
 public class Population {
@@ -12,6 +14,27 @@ public class Population {
         for (int i = 0; i < size; i++) {
             pop.add(new DNA(DNA.TARGET.length()));
         }        
+    }
+    
+    public ArrayList<DNA> createMatingPool(){
+        Random r = new Random();
+        ArrayList<DNA> matingPool = new ArrayList();
+        double maxFitness = 0;
+        
+        for (DNA dna : pop) {
+            if (dna.getFitness() > maxFitness) {
+                maxFitness = dna.getFitness();
+            }
+        }
+        for (DNA dna : pop) {
+            double fit = dna.getFitness() * (100 / maxFitness);
+            fit =(int) Math.floor(fit);
+            for (int i = 0; i < fit; i++) {
+                matingPool.add(dna);
+            }
+        }
+        
+        return  matingPool;
     }
             
     
